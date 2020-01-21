c36974c4c0e58528dc85c7a6e3e518a3642805e0
Duy <duyme18@gmail.com>
2020-01-21 17:50:30 +0700

    hidden-number-email-password-radio-select-textarea-file-multiple-

--- module/Form/src/Form/FormElement.php
+++ module/Form/src/Form/FormElement.php
@@ -15,7 +15,7 @@ class FormElement extends Form
         $fullname->setLabel('Fullname: ');
         $fullname->getLabelAttributes([
             'id' => 'fullname',
-            'class' => 'control-lable'
+            'class' => 'control-label'
         ]);
         $fullname->setAttributes([
             'type' => 'text',
@@ -27,7 +27,7 @@ class FormElement extends Form
         $this->add($fullname);
 
         //hidden
-        $hidden = new Element('hidden_input');
+        $hidden = new Element('hidden');
         $hidden->setAttributes([
             'type' => 'hidden',
             'value' => 'Zend Framework'
@@ -35,11 +35,11 @@ class FormElement extends Form
         $this->add($hidden);
 
         //number
-        $age = new Element\Number('number_input');
+        $age = new Element\Number('number');
         $age->setLabel('Chọn tuổi: ');
         $age->setLabelAttributes([
             'id' => 'age',
-            'class' => 'control-lable'
+            'class' => 'control-label'
         ]);
         $age->setAttributes([
             'min' => 10,
@@ -52,11 +52,11 @@ class FormElement extends Form
         $this->add($age);
 
         //email
-        $email = new Element\Email('email_input');
+        $email = new Element\Email('email');
         $email->setLabel('Nhập email: ')
             ->setLabelAttributes([
                 'id' => 'email',
-                'class' => 'control-lable'
+                'class' => 'control-label'
             ]);
         $email->setAttribute('class', 'form-control');
         $email->setAttributes([
@@ -66,5 +66,107 @@ class FormElement extends Form
         ]);
 
         $this->add($email);
+
+        //password
+        $password = new Element\Password('password');
+        $password->setLabel('Nhập password')
+            ->setLabelAttributes([
+                'id' => 'password',
+                'class' => 'control-label'
+            ]);
+        $password->setAttributes([
+            'class' => 'form-control',
+            'id' => 'password',
+            'placeholder' => "Nhập password: ",
+            'minlength' => 6,
+            'maxlength' => 20,
+            'required' => true
+        ]);
+
+        $this->add($password);
+
+        //radio
+        $gender = new Element\Radio('gender');
+        $gender->setLabel('Giới tính: ')
+            ->setLabelAttributes([
+                'id' => 'gender',
+                'class' => 'control-label'
+            ]);
+        $gender->setAttributes([
+            'id' => 'gender',
+            'required' => true,
+            'value' => 'nam',
+            'style' => 'margin-left: 20px'
+        ]);
+        $gender->setValueOptions([
+            'nam' => ' Nam',
+            'nữ' => ' Nữ',
+            'other' => ' Other'
+        ]);
+
+        $this->add($gender);
+
+        //select
+        $select = new Element\Select('select');
+        $select->setLabel('Chọn môn học: ')
+            ->setLabelAttributes([
+                'id' => 'select',
+                'class' => 'control-label'
+            ])
+            ->setAttributes([
+                'id' => 'select',
+                'class' => 'form-control'
+            ]);
+        $select->setValueOptions([
+            'php' => 'PHP',
+            'java' => 'JAVA',
+            'nodejs' => 'NODE JS'
+        ]);
+
+        $this->add($select);
+
+        //textarea
+        $textarea = new Element\Textarea('message');
+        $textarea->setLabel('Nhập nội dung: ');
+        $textarea->setLabelAttributes([
+            'id' => 'message',
+            'class' => 'control-label'
+        ])
+            ->setAttributes([
+                'rows' => 8,
+                'id' => 'message',
+                'class' => 'form-control',
+                'style' => 'resize:none'
+            ]);
+        $this->add($textarea);
+
+        //file
+        $file = new Element\File('file');
+        $file->setLabel('Chọn file: ')
+            ->setLabelAttributes([
+                'id' => 'file',
+                'class' => 'control-label'
+            ])
+            ->setAttributes([
+                'multiple' => true,
+                'class' => 'form-control'
+            ]);
+        $this->add($file);
+
+        //checkbox
+
+        $checkbox = new Element\Checkbox('checkbox');
+        $checkbox->setLabel('Remember me: ');
+        $checkbox->setLabelAttributes([
+            'id' => 'checkbox',
+            'class' => 'control-label'
+        ]);
+        $checkbox->setAttributes([
+            'id' => 'checkbox',
+        
+            'checked' => true
+        ]);
+
+        $this->add($checkbox);
     }
 }
